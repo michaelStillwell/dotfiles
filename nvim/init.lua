e475a6cb9ad63cb9da93ddf1a0d6cc4f7239a6ac
msti <contact@mstillwell.dev>
2024-04-21 01:28:28 -0500

    feat: added convenience window bindings

--- nvim/init.lua
+++ nvim/init.lua
@@ -201,6 +201,12 @@ end)
 -- add execute to file
 vim.keymap.set('n', '<leader>x', '<cmd>!chmod +x %<CR>', { silent = true })
 
+-- window movement because lazy
+vim.keymap.set('n', '<C-k>', '<C-w>k', { silent = true })
+vim.keymap.set('n', '<C-j>', '<C-w>j', { silent = true })
+vim.keymap.set('n', '<C-h>', '<C-w>h', { silent = true })
+vim.keymap.set('n', '<C-l>', '<C-w>l', { silent = true })
+
 -- scrolling
 vim.keymap.set('n', '<C-i>', '<C-d>zz')
 vim.keymap.set('n', '<C-u>', '<C-u>zz')
